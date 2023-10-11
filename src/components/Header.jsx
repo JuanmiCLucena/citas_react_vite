7e5161c86b73fb885516723ca67923c353b0e8ff
Juanmi <jmcostalucena2000@gmail.com>
2023-10-11 11:12:38 +0200

    Trabajo Honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <h1 className="text-5xl font-black text-center md:w-2/3 mx-auto">
-        Seguimiento Pacientes{" "}
+        Seguimiento Pacientes Con Github {""}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
